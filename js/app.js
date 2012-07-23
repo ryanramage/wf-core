b623532f1edb8535495f0b12d68eb7c383cfbdfe
default_git_name <default_git_email>
2012-07-23 03:40:16 +0000

    More

--- js/app.js
+++ js/app.js
@@ -1,40 +1,49 @@
 define('js/app', ['simple-uuid'], function(uuid){
 	var exports = {};
 	exports.generateSheet = function(businessId, lastSheetOrder, count) {
-			var codes = [];
-			for (var i=0; i < count; i++) {
-				var code = uuid.uuid();
-				var shortCode = exports.shortCode(code);
-				codes.push({
-					code: code,
-					shortCode : shortCode
-				});
-			}
 			var sheet = {};
 			sheet.generatedDate = new Date().getTime();
 			sheet.businessId = businessId;
 			sheet.sheetOrder = lastSheetOrder+1;
-			sheet.codes = codes;
+			sheet.codes = [];
+			for (var i=0; i < count; i++) {
+				var code = uuid.uuid();
+				var punchCode = exports.punchCode(code, sheet.sheetOrder);
+				sheet.codes.push({
+					code: code,
+					punchCode : punchCode
+				});
+			}
 			return sheet;
 	}
 	
 	
-	exports.shortCode = function(code) {
-		return code.substring(0, 8).replace('0', 'X');	
+	exports.punchCode = function(code, sheetOrder) {
+		return code.substring(0, 8).replace(/0/g, 'X').replace(/1/g, 'R') + '-' + sheetOrder;	
 	}
+
+	exports.splitPunchCode = function(punchCode) {
+		var codes =  punchCode.split('-');
+		return {
+			punchCode : codes[0],
+			sheetOrder : codes[1]
+		}
+	}
+
 	
 
-	exports.addPunchToCard = function(card, code) {
+	exports.addPunchToCard = function(card, punchCode) {
 		if (!card.punches) card.punches = [];
 		card.punches.push(code);
 	}
 
 
 	exports.preValidate = function(card) {
-		for (var i=0; i < card.punches.lengh; i++) {
-		
+		for (var i=0; i < card.punches.length; i++) {
+			card.punches[i] = splitPunchCode(card.punches[i]);
 		}			
 	}
 		
   return exports;
+
 });
